1c287ae83e8381fe3e7319ad22a1a03a8d8a5b58
Pedro Henrick Santos de Souza <58045591+pedroh-s@users.noreply.github.com>
2022-12-19 20:46:40 -0300

    Create script-funcionarios.sql
    Script utilizado para inclusão de funcionários , criação por  @pedroh-s , edição por @cleversonasj

---
+++ script-funcionarios.sql
@@ -0,0 +1,19 @@
+-- script para inclusão de funcionarios -- 
+
+
+insert into tb_funcionarios values('14435945045','Ellis','12/02/1982','09/09/2002','45080024543','Vendedora interna',2300.00,'Casada',false,28921772242,'F','jellis4@sciencedirect.com');
+insert into tb_funcionarios values('95981352094','Phillips','11/09/1976','01/12/2013','38982244220','Gerente de vendas',5000.00,'Solteiro',true,4737321474,'M','bphillips5@time.com');
+insert into tb_funcionarios values('80924341040','Katy', '02/01/1982','07/12/2009','32670359861','Gerente de estoque',6000.00,'Solteiro',true,61932520354,'F','katy2@woothemes.com');
+insert into tb_funcionarios values('62101531003','Harris','11/09/2000','08/12/2023','95058267835','Vendedora',2300.00,'Solteira',true,79935187068,'F','aharris7@ucoz.com');
+insert into tb_funcionarios values('19563333047','James','11/04/1984','09/7/2010','24906787180','Farmaceutico',5000.00,'Solteiro',false,82928637742,'M','rjames8@prnewswire.com');
+insert into tb_funcionarios values('94353308009','Jacobs','04/09/1971','07/11/2003','62847012599','Coordenadora Execultiva',15000.00,'Solteira',true,3495134744,'F','jjacobsa@sbwire.com');
+insert into tb_funcionarios values('26370144070','Black','12/05/1994','02/04/2023','04293056879','Seguranca',4000.00,'Solteiro',false,8625148108,'M','mblackb@edublogs.org');
+insert into tb_funcionarios values('17389947080','Schmidt','03/08/1999','10/13/2020','88027586840','Desenvolvedor Front-end',6000.00,'Viuvo',true,61933261033,'M','sschmidtc@state.gov');
+insert into tb_funcionarios values('63141985006','Webb','04/03/1969,','02/10/2006','32191979845','Farmaceutico chefe',6000.00,'Solteira',false,79922449776,'F','awebbd@baidu.com');
+insert into tb_funcionarios values('93842012004','Jacobs','01/07/1999','03/04/2022','27455368390','Tecnica em TI',4000.00,'Solteira',true,34932172671,'F','ajacobse@google.it');
+insert into tb_funcionarios values('32787407059','Medina','05/05/1978','04/03/2008','14895683366','Vendedora',3000.00,'Casada',true,82931354366,'F','smedinaf@amazonaws.com');
+insert into tb_funcionarios values('32845467079','Morgan','08/08/1978','05/04/2011','77406501736','Gerente de vendas',4000.00,'Solteiro',true,98925832076,'M','dmorgang@123-reg.co.uk');
+insert into tb_funcionarios values('22805591020','Nguyen','05/12/1980','11/03/2014','65444150604','Vendedor',3000.00,'Solteiro',false,34988567893,'M','jnguyenh@google.com');
+insert into tb_funcionarios values('59857602002','Day','01/08/1988','02/09/2014','72735928627','Vendedor',3000.00,'Casado',true,3837736241,'M','rdayi@chronoengine.com');
+insert into tb_funcionarios values('39512988046','Bryant','08/12/1975','08/12/2005','13606471403','Seguranca',5000.00,'Solteira',true,21933455325,'F','sbryantk@wunderground.com');
+insert into tb_funcionarios values('52132131006','Alexandra','08/12/1978','08/12/2005','13666672402','Seguranca',5000.00,'Solteiro',true,31231232146,'M','alexandra@marketwatch.com');
