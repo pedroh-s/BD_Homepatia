15140919aa73ccedf9c71d0ef05496a890647eb7
Pedro Henrick Santos de Souza <58045591+pedroh-s@users.noreply.github.com>
2022-12-19 21:28:53 -0300

    Create script-convenios.sql
    Scripts criado por @pedroh-s e conferido por @pedroventura, rodado com sucesso no projeto de BD

---
+++ script-convenios.sql
@@ -0,0 +1,13 @@
+-- script criação dos convenio --
+
+insert into tb_convenios values('Unimed', 'Central Nacional Unimed (CNU) é uma operadora de planos de saúde de cobertura nacional.', 120.00);
+insert into tb_convenios values('SulAmerica', 'Plano de de saude regional e ou nascional', 159.00);
+insert into tb_convenios values('Bradesco', 'Plano de saude do Bradesco', 100.00);
+insert into tb_convenios values('Amil', 'Plano de saude Amil, uma empresa muito respeitada no ramo', 200.00);
+insert into tb_convenios values('Notredame', 'NotreDame Intermédica, também conhecido como GNDI, é umas das principais operadoras de saúde do país', 300.00);
+insert into tb_convenios values('Porto Seguro', 'O seguro Porto Seguro aceito em todo territorio nascional', 230.00);
+insert into tb_convenios values('Prevent Senior', 'A Prevent Senior é especialista em planos de saúde e pioneira no atendimento para a melhor idade', 320.00);
+insert into tb_convenios values('Hapvida', 'Há mais de 40 anos no mercado de saúde, atualmente a Hapvida é consolidada como uma das maiores operadoras de saúde do Brasil', 230.00);
+insert into tb_convenios values('Omint', 'A Omint tem como foco principal a qualidade em seus serviços, e a excelência no atendimento humanizado', 290.00);
+insert into tb_convenios values('Amil One', 'A One Health oferece uma experiência única para seus beneficiários, através de um modelo moderno de atendimento, somado à medicina de ponta e serviços extraclasse, unindo conveniência com sofisticação', 350.00);
+
