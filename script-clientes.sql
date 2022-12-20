b42e1d5c9283fa6531321679b48b689a80e630c4
Pedro Henrick Santos de Souza <58045591+pedroh-s@users.noreply.github.com>
2022-12-19 21:26:51 -0300

    Create script-clientes.sql
    Script criado por @pedroh-s , editado pelo menos

---
+++ script-clientes.sql
@@ -0,0 +1,23 @@
+-- script criação clientes --
+
+
+insert into tb_cliente values('15994239798', 'Isabel Silvana Betina Assis', 'F', 11998199940, 'isabel-assis79@mucoucah.com.br');
+insert into tb_cliente values('60013997513', 'Nelson Roberto Cauã Carvalho', 'M', 98999274330, 'nelson_roberto_carvalho@carvajal.com');
+insert into tb_cliente values('74946788328', 'Pedro Danilo Rodrigues', 'M', 94983717330, 'pedro_danilo_rodrigues@unifesp.br');
+insert into tb_cliente values('64215634067', 'Elisa Yasmin Baptista', 'F', 81998452040, 'elisa_yasmin_baptista@sdrifs.com.br');
+insert into tb_cliente values('34820127764', 'Ester Louise Viana', 'F', 61991312732, 'ester.louise.viana@helponline-sti.com');
+insert into tb_cliente values('25426678618', 'Rosa Luciana da Cruz', 'F', 34995194607, 'rosalucianadacruz@superigi.com.br');
+insert into tb_cliente values('35888737089', 'Marcos Vinicius Martin Ramos', 'M', 86985539513, 'marcos.vinicius.ramos@rabelloadvogados.com.br');
+insert into tb_cliente values('63015368869','Tiago Victor da Rosa', 'M', 96984238061, 'tiagovictordarosa@ig.com');
+insert into tb_cliente values('52474197150', 'Esther Aparecida Marina Duarte', 'F', 6527773100, 'esther.aparecida.duarte@nipnet.com.br');
+insert into tb_cliente values('56309394401', 'Márcio Iago Barros', 'M', 6339892818, 'marcioiagobarros@gmx.com');
+insert into tb_cliente values('42028227141', 'Isabel Rosa Raquel Carvalho', 'F', 8429223995, 'isabelrosacarvalho@carreira.com.br');
+insert into tb_cliente values('55457072550','Marcos Vinicius Renato Pedro Oliveira', 'M', 32996262963, 'marcosviniciusoliveira@gmail.com');
+insert into tb_cliente values('23580336932', 'Larissa Laura Corte Real', 'F', 51992216211, 'larissa_laura_cortereal@senioraereo.com.br');
+insert into tb_cliente values('36779764566', 'Thales Guilherme José Campos', 'M', 86982655682, 'thales_campos@platinium.com.br' );
+insert into tb_cliente values('45330561132', 'Sônia Stefany Ferreira', 'F', 79998475581, 'soniastefanyferreira@carreira.com.br');
+insert into tb_cliente values('27695492515', 'Mirella Natália Brenda da Luz', 'F', 71992843599, 'mirella-daluz85@tricoproducts.com.br');
+insert into tb_cliente values('80788972510', 'Pietro Bernardo Samuel Farias', 'M', 4835125090, NULL); 
+insert into tb_cliente values('66850002872', 'Pietro Fábio Barros','M', 65993303981, 'pietro_barros@tecvap.com.br');
+insert into tb_cliente values('22430247569', 'Giovanni Mário Martins', 'M', 81993227994, 'giovanni_martins@estevao.ind.br');
+insert into tb_cliente values('00239912322', 'Cláudia Brenda Melo', 'F', 61984910587, 'claudia-melo74@oxiteno.com');
