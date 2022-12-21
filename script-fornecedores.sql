1f7dd9ca7b23e0591e19300870a1ae8aedf9a699
Pedro Henrick Santos de Souza <58045591+pedroh-s@users.noreply.github.com>
2022-12-21 19:28:40 -0300

    Create script-fornecedores.sql

---
+++ script-fornecedores.sql
@@ -0,0 +1,23 @@
+--script de fornecedores--
+insert into tb_fornecedores values('19282849000123','Nicolas e Pedro Henrique Herbalismo Ltda', 11987687501, 'fabricacao@nicolasepedrohenriqueconstrucoesltda.com.br');
+insert into tb_fornecedores values('24342926000123','Liz e Tomás Embalagens ME', 11983717235, 'almoxarifado@lizetomasembalagensme.com.br');
+insert into tb_fornecedores values('75520808000107', 'Rodrigo e Louise Plantas Ltda', 1136767097, 'compras@rodrigoelouiseplantas.com.br');
+insert into tb_fornecedores values('09541658000185', 'Lara e Helena Publicidade e Propaganda Ltda', 1128482283, 'fiscal@laraehelenapublicidadeepropagandaltda.com.br');
+insert into tb_fornecedores values('41938639000108', 'Geraldo e Gabrielly Flores ME', 11996856187, 'contato@geraldoegabriellyfloresme.com.br');
+insert into tb_fornecedores values('68138594000135','Vinicius e Bianca Alimentos LTDA', 3139611184, 'compras@viniciusebiancaalimentosltda.com.br');
+insert into tb_fornecedores values('13299907000145', 'Miguel e Anthony Insumos ME', 31986288754, 'manutencao@migueleanthonyinsumosme.com.br');
+insert into tb_fornecedores values('77540923000160', 'Laís e Manuela Limpeza ME', 3137018096, 'fiscal@laisemanuelalimpezame.com.br');
+insert into tb_fornecedores values('07715799000141', 'Lavínia e Cauê Plantas Ltda', 15993552742, 'producao@laviniaecaueplantasltda.com.br');
+insert into tb_fornecedores values('06698518000127', 'Isis e Yago Embalagens ME', 19989063212, 'fabricacao@isiseyagoembalagensme.com.br');
+insert into tb_fornecedores values('82109199000155', 'Gabriel e Rafael Ervas e Suprimentos ME', 2736285123 ,'treinamento@gabrielerafaelervasme.com.br');
+insert into tb_fornecedores values('79760372000176', 'Rosa e Flávia Comercio de especiarias ME',2726185953, 'atendimento@rosaflaviaespeciarias.com.br');
+insert into tb_fornecedores values('01064438000104', 'Theo e César Alimentos Ltda', 2736273936, 'almoxarifado@theoecesaralimentosltda.com.br');
+insert into tb_fornecedores values('10383261000191', 'Ricardo e Carolina Distribuidora ME', 27988838608, 'contato@ricardoecarolinadistribuidorame.com.br');
+insert into tb_fornecedores values('56410616000195', 'Maitê e Vitória Ervas Ltda', 2737192302, 'producao@maiteevitoriaervasltda.com.br' );
+insert into tb_fornecedores values('31249147000143', 'Gourmet Produtos Alimenticio Ltda', 2725342708, 'ti@gourmetprodutosalimentos.com.br');
+insert into tb_fornecedores values('11872292000179', 'Kevin e Luiz plantas Ltda', 9237003783, 'pedido@kevinluizplantasltda.com.br');
+insert into tb_fornecedores values('48046140000127', 'Isabella e Catarina Telecom ME', 9237003783, 'ti@isabellaecatarinatelecomme.com.br');
+insert into tb_fornecedores values('48973497000150','Geraldo e Alessandra Gráfica ME', 92996094808, 'ouvidoria@geraldoealessandragraficame.com.br');
+insert into tb_fornecedores values('16561245000128','Sebastião e Levi Alimentos ME', 9237342987, 'fiscal@sebastiaoelevialimentosme.com.br');
+
+select * from tb_fornecedores tf ;
